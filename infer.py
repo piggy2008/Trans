852469cd328523174acd1250490d68ba3de426d7
piggy2008 <810656271@qq.com>
2021-06-04 10:13:51 +0800

    first test for transformer

--- infer.py
+++ infer.py
@@ -33,7 +33,7 @@ args = {
     'snapshot': '200000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
-    'input_size': (380, 380),
+    'input_size': (224, 224),
     'start': 40000
 }
 
