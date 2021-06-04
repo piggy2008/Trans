5243dedd1e007c761ea36609e2a3490ec87b6df9
piggy2008 <810656271@qq.com>
2021-06-04 10:13:17 +0800

    first test for transformer

--- infer.py
+++ infer.py
@@ -26,7 +26,7 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = saving_path
 
-exp_name = 'VideoSaliency_2021-06-03 21:01:523'
+exp_name = 'VideoSaliency_2021-06-03 21:01:52'
 
 args = {
     'gnn': True,
