0099aed611b0ccb8a586ca9cb2cf980c020e3363
piggy2008 <810656271@qq.com>
2021-06-08 10:14:58 +0800

    convTrans

--- infer.py
+++ infer.py
@@ -128,7 +128,7 @@ def main(snapshot):
                     flow_var = Variable(img_transform(flow).unsqueeze(0), volatile=True).cuda()
                     start = time.time()
 
-                    prediction, prediction2, prediction3 = net(img_var, flow_var)
+                    prediction, prediction3 = net(img_var, flow_var)
                     prediction = torch.sigmoid(prediction)
 
                     end = time.time()
@@ -152,7 +152,7 @@ def main(snapshot):
 
                     start = time.time()
 
-                    prediction, prediction2, prediction3 = net(img_var, flow_var)
+                    prediction, prediction3 = net(img_var, flow_var)
                     prediction = torch.sigmoid(prediction3)
 
                     end = time.time()
