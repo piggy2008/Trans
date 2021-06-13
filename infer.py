ffc0f063530557182a9aeacca3533782bffac2f0
piggy2008 <810656271@qq.com>
2021-06-13 10:18:06 +0800

    multi-head attention

--- infer.py
+++ infer.py
@@ -26,7 +26,7 @@ torch.cuda.set_device(device_id)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = saving_path
 
-exp_name = 'VideoSaliency_2021-06-09 15:09:57'
+exp_name = 'VideoSaliency_2021-06-12 11:57:49'
 
 args = {
     'gnn': True,
