3c803e84aae07aebd7254b115487c521bf7d72b9
piggy2008 <810656271@qq.com>
2021-06-19 20:58:30 +0800

    attention=2 + multiply loss

--- infer.py
+++ infer.py
@@ -46,10 +46,10 @@ to_pil = transforms.ToPILImage()
 # to_test = {'ecssd': ecssd_path, 'hkuis': hkuis_path, 'pascal': pascals_path, 'sod': sod_path, 'dutomron': dutomron_path}
 # to_test = {'ecssd': ecssd_path}
 
-to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
-gt_root = os.path.join(davis_path, 'GT')
-flow_root = os.path.join(davis_path, 'flow')
-imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
+# to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
+# gt_root = os.path.join(davis_path, 'GT')
+# flow_root = os.path.join(davis_path, 'flow')
+# imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 
 # to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
 # gt_root = os.path.join(fbms_path, 'GT')
@@ -70,10 +70,10 @@ imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
 # flow_root = os.path.join(vos_path, 'flow')
 # imgs_path = os.path.join(vos_path, 'VOS_test_single.txt')
 
-# to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
-# gt_root = os.path.join(davsod_path, 'GT')
-# flow_root = os.path.join(davsod_path, 'flow')
-# imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
+to_test = {'DAVSOD': os.path.join(davsod_path, 'DAVSOD_test')}
+gt_root = os.path.join(davsod_path, 'GT')
+flow_root = os.path.join(davsod_path, 'flow')
+imgs_path = os.path.join(davsod_path, 'DAVSOD_test_single.txt')
 
 # to_test = {'MCL': os.path.join(mcl_path, 'MCL_test')}
 # gt_root = os.path.join(mcl_path, 'GT')
